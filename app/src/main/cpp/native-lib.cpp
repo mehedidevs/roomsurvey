536a8a4c338b31f602fdd153d5ad6f559f091afb
masum_devs <mehedieasital@gmail.com>
2024-01-11 16:36:00 +0600

    initial commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,11 +8,8 @@ Java_com_mehedi_bedaroomtest_MainActivity_apiUrl(
         JNIEnv *env,
         __attribute__((unused)) jclass clazz
 ) {
-//    std::string url = "https://legacy.beedamall.com/api/";
-    std::string url = "https://api.beeda.com/";
-//    std::string url = "http://192.168.88.199/";
-//    std::string url = "http://192.168.88.221/beeda_backend/api/";
-//    std::string url = "http://192.168.88.243/beeda_backend/api/";
-//    std::string url = "http://192.168.88.234/beeda_backend/api/";
+
+    std::string url = "";
+
     return env->NewStringUTF(url.c_str());
 }
